650772b064581971acc17c5efc2d9f3efa12b398
Faisal Khan <faisal.khan@webmobriltechnologies.com>
2022-10-25 13:55:47 +0530

    add new design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,8 @@
-import { Component, ComponentRef, OnDestroy, ViewChild } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { AlertComponent } from './alert/alert.component';
-import { PlaceholderDirective } from './placeholder.directive';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnDestroy {
-  sub?: Subscription;
-  @ViewChild(PlaceholderDirective) host!: PlaceholderDirective;
-  title = 'ngapp1';
-
-  constructor() {}
-
-  showAlert() {
-    this.host.vcr.clear();
-    let comp = this.host.vcr.createComponent(AlertComponent);
-    comp.instance.msg = 'Hello From Dynamic';
-    this.sub = comp.instance.close.subscribe((rs) => {
-      if (this.sub) this.sub.unsubscribe();
-      this.host.vcr.clear();
-    });
-  }
-
-  ngOnDestroy(): void {
-    if (this.sub) this.sub.unsubscribe();
-  }
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UnlessDirective } from './unless.directive';
-import { AlertComponent } from './alert/alert.component';
-import { PlaceholderDirective } from './placeholder.directive';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    UnlessDirective,
-    AlertComponent,
-    PlaceholderDirective
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
